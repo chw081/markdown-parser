ef97a9aa36e6781ccfc0aea088c6cdd99fdb7c17
chw081 <chw081@ucsd.edu>
2022-04-23 20:33:47 -0700

    updateCurrentIndex

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,8 +17,11 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             if (openBracket > -1 && closeBracket > 1 && openParen > -1 && closeParen > -1) {
-                    toReturn.add(markdown.substring(openParen + 1, closeParen));
-                    currentIndex = closeParen + 1;
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            else {
+                currentIndex ++;
             }
         }
 
