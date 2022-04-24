596930e80f3c991b78f2efc48fd0d7c28ea2b19f
chw081 <chw081@ucsd.edu>
2022-04-23 20:31:24 -0700

    checkBracketParenExist

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,8 +16,10 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if (openBracket > -1 && closeBracket > 1 && openParen > -1 && closeParen > -1) {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    currentIndex = closeParen + 1;
+            }
         }
 
         return toReturn;
