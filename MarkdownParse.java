1d686a1f7b42a85cf9e2fba66048370e0a73608e
chw081 <chw081@ucsd.edu>
2022-05-08 18:19:24 -0700

    deleting print statement

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
-        System.out.println("Hello World!");
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
